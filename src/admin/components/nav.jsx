20feb31a08c02e65df195f0b3d59c854081e7dae
김미나 <20261202@dsm.hs.kr>
2026-09-15 16:51:45 +0900

    refactor: Nav 스타일 분리 및 Logo 컴포넌트 수정

--- src/admin/components/nav.jsx
+++ src/admin/components/nav.jsx
@@ -5,7 +5,8 @@ const Nav = () => {
   return (
     <Sidebar>
       <SidebarNav>
-        <p>건널까말까</p>
+        <Logo>건널까말까</Logo>
+
         <NavLink to="/admin">대시보드</NavLink>
         <NavLink to="/state">상태</NavLink>
         <NavLink to="/event">이벤트 로그</NavLink>
@@ -30,6 +31,13 @@ const Sidebar = styled.aside`
   flex-direction: column; // 세로방향으로~
 `;
 
+const Logo = styled.p`
+    margin: 15px 0px;
+    font-size: 25px;
+    font-weight: 700;
+    padding-left: 16px;
+`
+
 const SidebarNav = styled.nav`
   // 꽉채우기
   height: 100%;
@@ -37,13 +45,6 @@ const SidebarNav = styled.nav`
   flex: 1; // 로그아웃과 그. ㅚ. 으ㅣ 것들 사이의 공간을 만들어 주기 위한 여백코드~
   display: flex;
   flex-direction: column;
-
-  p {
-    margin: 15px 0px;
-    font-size: 25px;
-    font-weight: 700;
-    padding-left: 16px;
-  }
 
   a {
     display: block;
@@ -65,7 +66,6 @@ const SidebarNav = styled.nav`
     margin-top: auto;
     margin-bottom: 0;
     color: red;
-    background-color: #ffeeee;
   }
   a.logout:hover {
     background-color: #ffc7c7;
